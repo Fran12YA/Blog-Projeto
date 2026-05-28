2e20a1a9944b56bbb539f3acc5e2145519168570
Francielly Dantas <dantasfrancielly5@exemplo.com>
2026-05-28 10:45:29 -0300

    Exemplos de displays

--- exemplos/views.py
+++ exemplos/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def exemplos_bootstrap_view(request):
     # Lembre-se de colocar o HTML na pasta: exemplos/templates/exemplos/template_bootstrap.html
-    return render(request, 'exemplos/colapse.html')
+    return render(request, 'exemplos/display_flex_three.html')
